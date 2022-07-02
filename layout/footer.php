fdbccee3a64c87b12a6ff8d0df44122e5631b1a0
rakhax64 <95220908+rakhax64@users.noreply.github.com>
2022-07-02 11:21:54 +0700

    Home Page

---
+++ layout/footer.php
@@ -0,0 +1,29 @@
+<div class="row bg-purple-darken font-white" id="footer">
+    <div class="section">
+        <br>
+        <div class="col-md-5">
+            <h2 class="font-bold font-lg">Kantor Pusat</h2>
+            <section class="line"></section>
+            <p class="font-sm font-medium"><i class="fa fa-map-marker"></i> Gedung PERHATI Lt. 3 <br>Jl. KemanaAja Jaya Gg XXII No 20, Surabaya</p>
+            <p class="font-sm font-medium"><i class="fa fa-phone"></i> (+6321) 404 2132</p>
+            <p class="font-sm font-medium"><i class="fa fa-whatsapp"></i> +62 822-3333-0808 (Hotline)</p>
+            <p class="font-sm font-medium"><i class="fa fa-envelope-o"></i> info@zakat.org</p>
+        </div>
+        <div class="col-md-7 txt-right">
+            <h2 class="font-bold font-lg pb-3">Kunjungi Sosial Media Kami</h2>
+            <a href="" class="follow hvr-rectangle-out"><i class="fa fa-instagram fa-2x"></i></a>
+            <a href="" class="follow hvr-rectangle-out"><i class="fa fa-facebook fa-2x"></i></a>
+            <a href="" class="follow hvr-rectangle-out"><i class="fa fa-twitter fa-2x"></i></a>
+            <a href="" class="follow hvr-rectangle-out"><i class="fa fa-youtube fa-2x"></i></a>
+        </div>
+        <div class="col-md-12 text-center">
+            <hr>
+        <!-- </div>
+        <div class="col-md-6">
+            <b>Copyright &copy; Lembaga Manajemen Infaq</b>
+        </div> -->
+        <div >
+            <b></b>
+        </div>
+    </div>
+</div>
